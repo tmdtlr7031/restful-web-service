84a985b881c4631dac5987cc2a4ebe17142dbf1f
namseungsik <tmd7031@naver.com>
2020-08-12 23:29:52 +0900

    JPA Dependency 부분, security 건너뛰고(?) 접근하게 설정

--- src/main/java/com/restfullec/restfulwebservice/config/SecurityConfig.java
+++ src/main/java/com/restfullec/restfulwebservice/config/SecurityConfig.java
@@ -3,10 +3,18 @@ package com.restfullec.restfulwebservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration // 스프링부트가 실행되면서 메모리에 설정정보를 같이 로딩함
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable(); // 크로스사이트 disable - 나중에 찾아보기
+        http.headers().frameOptions().disable(); // 헤더값에 frame관련값 사용 X - 나중에 찾아보기
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
